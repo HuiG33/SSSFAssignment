842f2c24040ffdf9831b74164a363ebb1d66d89e
HuiG33 <joni.tefke12@gmail.com>
2019-03-21 21:28:55 +0200

    clean

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,16 @@
 // JavaScript
 const map = L.map('map').setView([60.1699, 24.9384], 13);
+let marker = L.marker([60.1699, 24.9384]).addTo(map);
+marker.bindPopup("<b>Latitude: </b>60.1699<br><b>Longitude: </b>24.9384").openPopup();
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 map.on('click', (evt) => {
-  alert("Lat, Lon : " + evt.latlng.lat + ", " + evt.latlng.lng)
+  map.removeLayer(marker);
+  marker = L.marker([evt.latlng.lat, evt.latlng.lng]).addTo(map);
+  marker.bindPopup("<b>Latitude: </b>" + evt.latlng.lat + "<br><b>Longitude: </b>" + evt.latlng.lng).openPopup();
 });
 
 map.on('locationfound', (evt) => {
