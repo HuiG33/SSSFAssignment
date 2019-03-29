a794f6e3301c215480c8ec3bab43a9763deac686
HuiG33 <joni.tefke12@gmail.com>
2019-03-29 09:22:33 +0200

    redirect https

--- server.js
+++ server.js
@@ -9,10 +9,22 @@ const db = require('./modules/database');
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 db.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`, app);
+
+app.use ((req, res, next) => {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 app.use('/', eventRouter);
